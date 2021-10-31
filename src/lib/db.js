c0e6d8cf052b3578d4a0b2b4dd0329519a79cb92
Harish Ramachandra <anamethatistaken@gmail.com>
2021-10-31 10:15:30 +0000

    commit

---
+++ src/lib/db.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL=https://mbvfdhtzayitphafbctf.supabase.co,
+  import.meta.env.VITE_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNTY3Mzc5OSwiZXhwIjoxOTUxMjQ5Nzk5fQ.-mZ4NsRpT_-vwScM02GRTS2QKj4PLtKqGaLkvnRKelE;
+)
+
+export default supabase
